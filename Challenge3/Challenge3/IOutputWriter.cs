f0949d2e11242c89e6b626198d83bf315e76d3a7
albpb2 <albpb2@gmail.com>
2019-05-02 18:52:57 +0200

    Output writer.

---
+++ Challenge3/Challenge3/IOutputWriter.cs
@@ -0,0 +1,10 @@
+﻿using Challenge3.Model.Paper;
+using System.Collections.Generic;
+
+namespace Challenge3
+{
+    public interface IOutputWriter
+    {
+        void WriteOutput(List<Paper> cases, string outputPath);
+    }
+}
---
+++ Challenge3/Challenge3/OutputWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Challenge3.Model.Paper;
+
+namespace Challenge3
+{
+    public class OutputWriter : IOutputWriter
+    {
+        public void WriteOutput(List<Paper> cases, string outputPath)
+        {
+            var lines = BuildOutputLines(cases).SelectMany(l => l);
+            File.WriteAllLines(outputPath, lines);
+        }
+
+        private IEnumerable<List<string>> BuildOutputLines(List<Paper> cases)
+        {
+            for (var i = 0; i < cases.Count; i++)
+            {
+                var caseLines = new List<string>();
+                caseLines.Add($"Case #{i + 1}:");
+
+                foreach (var punch in cases[i].Punches.OrderBy(p => p.X).ThenBy(p => p.Y))
+                {
+                    caseLines.Add($"{punch.X} {punch.Y}");
+                }
+
+                yield return caseLines;
+            }
+        }
+    }
+}
