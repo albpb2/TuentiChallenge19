96d50f9d83df864e1beb16508874e80188001bf5
albpb2 <albpb2@gmail.com>
2019-05-02 17:16:08 +0200

    Missing files.

---
+++ Challenge2/Challenge2/IOutputWriter.cs
@@ -0,0 +1,10 @@
+﻿using Challenge2.Model;
+using System.Collections.Generic;
+
+namespace Challenge2
+{
+    public interface IOutputWriter
+    {
+        void WriteOutput(List<Case> cases, string outputPath);
+    }
+}
---
+++ Challenge2/Challenge2/OutputWriter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Challenge2.Model;
+
+namespace Challenge2
+{
+    public class OutputWriter : IOutputWriter
+    {
+        public void WriteOutput(List<Case> cases, string outputPath)
+        {
+            var lines = BuildOutputLines(cases);
+            File.WriteAllLines(outputPath, lines);
+        }
+
+        private IEnumerable<string> BuildOutputLines(List<Case> cases)
+        {
+            for (var i = 0; i < cases.Count; i++)
+            {
+                yield return $"Case #{i + 1}: {cases[i].SourcePlanet.CountPaths()}";
+            }
+        }
+    }
+}
