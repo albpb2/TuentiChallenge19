49ff9599ac30aed461b674b8cb5088e985ca4956
albpb2 <albpb2@gmail.com>
2019-05-03 18:50:06 +0200

    Algorithm to calculate candies averages.

---
+++ Challenge4/Challenge4/CandiesCalculator.cs
@@ -0,0 +1,33 @@
+﻿using Challenge4.Model;
+using System.Linq;
+
+namespace Challenge4
+{
+    public class CandiesCalculator
+    {
+        public (int candies, int people) CalculateAverageCandies(PartyList partyLists)
+        {
+            var groups = partyLists.List.GroupBy(element => element).ToDictionary(g => g.Key, g => g.Count());
+
+            var highestGroup = groups.Keys.Max();
+
+            var leastCommonMultiple = Maths.CalculateLeastCommonMultiple(highestGroup, groups[highestGroup]);
+            var listRepetitions = leastCommonMultiple;
+            
+            while (!groups.All(g => IsValidMultipleForGroup(listRepetitions, g.Key, g.Value)))
+            {
+                listRepetitions = listRepetitions + leastCommonMultiple;
+            }
+
+            var candies = groups.Sum(g => listRepetitions * g.Value);
+            var people = groups.Sum(g => (listRepetitions * g.Value) / g.Key);
+
+            return Maths.ReduceFraction(candies, people);
+        }
+
+        private static bool IsValidMultipleForGroup(int multiple, int candies, int repetitions)
+        {
+            return (multiple * repetitions) % 2 == 0;
+        }
+    }
+}
---
+++ Challenge4/Challenge4/Maths.cs
@@ -0,0 +1,55 @@
+﻿namespace Challenge4
+{
+    public static class Maths
+    {
+        public static int CalculateLeastCommonMultiple(int a, int b)
+        {
+            int num1, num2;
+            if (a > b)
+            {
+                num1 = a; num2 = b;
+            }
+            else
+            {
+                num1 = b; num2 = a;
+            }
+
+            for (int i = 1; i < num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
+                {
+                    return i * num1;
+                }
+            }
+            return num1 * num2;
+        }
+
+        public static int CalculateGreatestCommonDivisor(int a, int b)
+        {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a == 0 ? b : a;
+        }
+
+        public static (int, int) ReduceFraction(int numerator, int denominator)
+        {
+            var greatestCommonDivisor = Maths.CalculateGreatestCommonDivisor(numerator, denominator);
+
+            while (greatestCommonDivisor != 1)
+            {
+                numerator = numerator / greatestCommonDivisor;
+                denominator = denominator / greatestCommonDivisor;
+
+                greatestCommonDivisor = Maths.CalculateGreatestCommonDivisor(numerator, denominator);
+            }
+
+            return (numerator, denominator);
+        }
+    }
+}
