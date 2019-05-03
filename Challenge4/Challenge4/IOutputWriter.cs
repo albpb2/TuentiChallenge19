07c4fa020554a0f68c02b814710576d88a858bad
albpb2 <albpb2@gmail.com>
2019-05-03 18:56:20 +0200

    Create output writer.

---
+++ Challenge4/Challenge4/IOutputWriter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Challenge4
+{
+    public interface IOutputWriter
+    {
+        void WriteOutput(List<(int, int)> candiesAverages, string outputPath);
+    }
+}
---
+++ Challenge4/Challenge4/OutputWriter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Challenge4
+{
+    public class OutputWriter : IOutputWriter
+    {
+        public void WriteOutput(List<(int, int)> candiesAverages, string outputPath)
+        {
+            var lines = BuildOutputLines(candiesAverages);
+            File.WriteAllLines(outputPath, lines);
+        }
+
+        private IEnumerable<string> BuildOutputLines(List<(int Candies, int People)> candiesAverages)
+        {
+            for (var i = 0; i < candiesAverages.Count; i++)
+            {
+                yield return $"Case #{i + 1}: {candiesAverages[i].Candies}/{candiesAverages[i].People}";
+            }
+        }
+    }
+}
